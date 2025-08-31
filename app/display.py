64e5789d2e11b0ea641213afb1d50350ec24e6fc
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2025-08-31 16:09:30 +0300

    'Solution'

---
+++ app/display.py
@@ -0,0 +1,16 @@
+from models import Book
+
+
+class DisplayStrategy:
+    def display(self, book: Book) -> None:
+        raise NotImplementedError
+
+
+class ConsoleDisplay(DisplayStrategy):
+    def display(self, book: Book) -> None:
+        print(book.content)
+
+
+class ReverseDisplay(DisplayStrategy):
+    def display(self, book: Book) -> None:
+        print(book.content[::-1])
---
+++ app/printer.py
@@ -0,0 +1,18 @@
+from models import Book
+
+
+class PrintStrategy:
+    def print_book(self, book: Book) -> None:
+        raise NotImplementedError
+
+
+class ConsolePrinter(PrintStrategy):
+    def print_book(self, book: Book) -> None:
+        print(f"Printing the book: {book.title}...")
+        print(book.content)
+
+
+class ReversePrinter(PrintStrategy):
+    def print_book(self, book: Book) -> None:
+        print(f"Printing the book in reverse: {book.title}...")
+        print(book.content[::-1])
